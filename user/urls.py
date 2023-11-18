99617dc42f1a9be118ad3877ca5baeab511b8e16
Sasha Semenenko <semenenko233249@gmail.com>
2023-11-18 23:57:27 +0200

    Added possibility for users receiving token for authentication

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register-user"),
+    path("login/", CreateTokenView.as_view(), name="login")
 ]
 
 app_name = "user"
