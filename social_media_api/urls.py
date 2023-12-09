0302c15b5f607b4abd675616cf3151d203fdd607
Sasha Semenenko <semenenko233249@gmail.com>
2023-12-09 19:15:17 +0200

    Added api  documentation for social media api

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -16,10 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/user/profiles/", include("user.urls",  namespace="profiles")),
-    path("api/post-comments/", include("post_and_comments.urls", namespace="post-and-comments"))
+    path("api/post-comments/", include("post_and_comments.urls", namespace="post-and-comments")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
